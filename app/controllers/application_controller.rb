d0c3c9187c75a20fc2ee3131825ba443a9cc6dc4
sumit <mauryasumit222@gmail.com>
2024-02-22 18:50:44 +0530

    [v4] [22 Feb] [ Trying To Fix Code For Devise ]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_employee!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
+  protected
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      root_path
+    else
+      root_path
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
---
+++ db/migrate/20240222102224_addroleas_integer.rb
@@ -0,0 +1,5 @@
+class AddroleasInteger < ActiveRecord::Migration[7.1]
+  def change
+    add_column :employees, :role, :integer, default: 0
+  end
+end

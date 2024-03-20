5cda26d78dacd751f71e4a5b356bd9ad4495a704
sumit <mauryasumit222@gmail.com>
2024-03-20 11:17:01 +0530

    [ 20Feb ] [" Implementing Dashboard code using helper method "]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,5 @@
 class UsersController < ApplicationController
   def dashboard
     @user = current_user
-    @employees_present_count = User.employee_count
-    @total_employees_count = User.total_count
-    @pending_reviews_count = Status.pending_count
-    @resolved_reviews_count = Status.resolved_count
-    @issued_reviews_count = Status.issue_count
-    @completed_reviews_count = Status.completed_count
-    @total_reviews_count = Status.total_count
-    @current_employees_pending_reviews_count = current_user.pending_reviews_count
-    @current_employees_completed_reviews_count = current_user.completed_reviews_count
-    @current_employees_total_reviews_count = current_user.total_reviews_count
-    @logs = Log.all
-    @total_statuses_count = current_user.total_statuses_count
   end
 end
